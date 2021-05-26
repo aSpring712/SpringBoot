1d4af171715c716067fafea22fb28bfc6a817a95
aSpring <rnjsqhal2001@naver.com>
2021-05-26 23:35:57 +0900

    first commit

---
+++ src/main/java/com/dream/have/DoDreamApplication.java
@@ -0,0 +1,13 @@
+package com.dream.have;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/have/DoDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.have;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
